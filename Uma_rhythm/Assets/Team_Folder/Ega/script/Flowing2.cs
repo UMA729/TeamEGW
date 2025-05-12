72bc1f6032ac5b7ad9f1a5d1cf383ace56472a42
DESKTOP-CMFB67U\GA2B <h1202416002@hiratagakuen.onmicrosoft.com>
2025-05-12 16:32:00 +0900

    ウマ走り製作途中

---
+++ Uma_rhythm/Assets/Team_Folder/Ega/script/Flowing2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Flowing2 : MonoBehaviour
+{
+
+    public float speed;      //スピード
+    public float resetPosX = 10.0f; //どの位置で戻るか
+
+    private Vector3 OriginalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginalPos = transform.position; //初期位置を保存
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+
+        if (transform.position.x >= OriginalPos.x + resetPosX)
+        {
+            transform.position = OriginalPos;
+        }
+    }
+}

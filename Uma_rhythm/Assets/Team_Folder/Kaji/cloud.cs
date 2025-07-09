8e9c0b3807a8efc11225e8bfbc8619c60d426f40
DESKTOP-DJOMAJ1\GA2B <h1202419009@hiratagakuen.onmicrosoft.com>
2025-07-09 16:31:02 +0900

    ketu

--- Uma_rhythm/Assets/Team_Folder/Kaji/cloud.cs
+++ Uma_rhythm/Assets/Team_Folder/Kaji/cloud.cs
@@ -5,25 +5,45 @@ using UnityEngine.UI;
 
 public class cloud : MonoBehaviour
 {
+    [SerializeField] Sprite[] images;
+    [SerializeField] Image image;
     public RectTransform cloudman;
     public float img_speed;
     public float ResetPos;
-    Vector2 img_pos;
+    int RandomNumP = 0;
+    int RandomNumI = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        img_pos = cloudman.anchoredPosition;
-        cloudman.anchoredPosition = new Vector2(1110, 308);
+        RandomNumP = Random.Range(160, 460);
+        cloudman.anchoredPosition = new Vector2(1110, RandomNumP);
     }
 
     // Update is called once per frame
     void Update()
     {
         cloudman.anchoredPosition -= new Vector2(img_speed, 0) * Time.deltaTime;
+
         if (cloudman.anchoredPosition.x <= ResetPos)
         {
-            cloudman.anchoredPosition = img_pos;
+            cloudman.anchoredPosition = new Vector2(1110, RandomNumP);
+
+            RandomNumP = Random.Range(160, 460);
+            RandomNumI = Random.Range(0, 100);
+            if (0 < RandomNumI && RandomNumI < 70)
+            {
+                RandomNumI = 0;
+            }
+            else if (70 <= RandomNumI && RandomNumI <= 98)
+            {
+                RandomNumI = 1;
+            }
+            else
+            {
+                RandomNumI = 2;
+            }
+        image.overrideSprite = images[RandomNumI];
         }
 
     }

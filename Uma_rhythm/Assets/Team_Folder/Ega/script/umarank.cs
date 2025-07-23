4280efdcc8d0bc703f7237d57ee6715082985342
DESKTOP-U0TU0SG\GA2B <h1202415020@hiratagakuen.onmicrosoft.com>
2025-07-23 16:09:30 +0900

    うまの挙動変えてみた感想プリズ

--- Uma_rhythm/Assets/Team_Folder/Ega/script/umarank.cs
+++ Uma_rhythm/Assets/Team_Folder/Ega/script/umarank.cs
@@ -7,21 +7,24 @@ public class umarank : MonoBehaviour
     public GameObject[] uma;
     [SerializeField] Transform[] PtoG;
 
-
     int rnd = 0;
+    float min = 0;
+    float min1 = 0;
+    float max = 0;
+    float max1 = 0;
 
     public int speedmin = 0;
     public int speedmax = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    public int speedmin2 = 0;
+    public int speedmax2 = 0;
     int SetRandom()
     {
         rnd = Random.Range(speedmin, speedmax);
+        return rnd;
+    }
+    int SetRandom2()
+    {
+        rnd = Random.Range(speedmin2, speedmax2);
         return rnd;
     }
 
@@ -29,8 +32,22 @@ public class umarank : MonoBehaviour
     {
         for(int i=0;i<=1;i++)
         {
-               Speed = SetRandom();
-        uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, -Speed * Time.deltaTime);
+            if(150>max && i == 0)
+            {
+                Speed = SetRandom2();
+                max += Speed;
+                min -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, -Speed * Time.deltaTime);
+                
+            }
+            if(150>max1 && i == 1)
+            {
+                Speed = SetRandom();
+                max1 += Speed;
+                min1 -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, -Speed * Time.deltaTime);
+            }
+               
         }
    
         Debug.Log("パフェ");
@@ -39,8 +56,21 @@ public class umarank : MonoBehaviour
     {
         for (int i = 0; i <= 1; i++)
         {
-            Speed = SetRandom();
-            uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, -Speed * Time.deltaTime);
+            if (150 > max && i == 0)
+            {
+                Speed = SetRandom2() + 1;
+                max += Speed;
+                min -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, -Speed * Time.deltaTime);
+
+            }
+            if (150 > max1 && i == 1)
+            {
+                Speed = SetRandom() + 1;
+                max1 += Speed;
+                min1 -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, -Speed * Time.deltaTime);
+            }
         }
 
         Debug.Log("パフェ");
@@ -49,8 +79,21 @@ public class umarank : MonoBehaviour
     {
         for (int i = 0; i <= 1; i++)
         {
-            Speed = SetRandom();
-            uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, Speed * Time.deltaTime);
+            if (300 > min && i == 0)
+            {
+                Speed = SetRandom() - 1;
+                min += Speed;
+                max -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, Speed * Time.deltaTime);
+
+            }
+            if (300 > min1 && i == 1)
+            {
+                Speed = SetRandom2() - 1;
+                min1 += Speed;
+                max1 -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, Speed * Time.deltaTime);
+            }
         }
 
         Debug.Log("パフェ");
@@ -59,8 +102,21 @@ public class umarank : MonoBehaviour
     {
         for (int i = 0; i <= 1; i++)
         {
-            Speed = SetRandom();
-            uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, Speed * Time.deltaTime);
+            if (300 > min && i == 0)
+            {
+                Speed = SetRandom();
+                min += Speed;
+                max -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, Speed * Time.deltaTime);
+
+            }
+            if (300 > min1 && i == 1)
+            {
+                Speed = SetRandom2();
+                min1 += Speed;
+                max1 -= Speed;
+                uma[i].transform.position = Vector3.MoveTowards(uma[i].transform.position, PtoG[i].position, Speed * Time.deltaTime);
+            }
         }
 
         Debug.Log("パフェ");
